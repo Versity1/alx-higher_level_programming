d5bec89ff9fda32a96697fd475154a3c82e979bf
Versity ICT Solutions <90280527+Versity1@users.noreply.github.com>
2023-02-15 18:08:34 +0100

    Update 8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
--- This script display the number of records with id= 89
-
-SELECT COUNT (id) FROM first_table WHERE id = 89;
-
+-- Displays the number of records with id = 89 in the table first_table.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
